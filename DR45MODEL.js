13cc3d52494c28ca3c64479675c54f0c021fe4ae
dhurias <hcps-dhurias@henricostudents.org>
2025-01-29 19:22:24 -0500

    Update DR45MODEL.js

--- DR45MODEL.js
+++ DR45MODEL.js
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link href="./DR45CSS.css" rel="stylesheet">
-    <script src="./DR45MODEL.js" defer></script>
-    <script src="./DR45VIEW.js" defer></script>
-    <script src="./DR45CONTROLLER.js" defer></script>
-  </head>
-</html>
+
