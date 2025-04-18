e24cfef67b4c3304e7e406f593989258cfb544e8
Sheryl D <hcps-dhurias@henricostudents.org>
2025-04-18 09:09:15 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    DB_HOST = "drhscit.org"
-    DB_NAME = "dr45"
-    DB_USER = "dr45"
-    DB_PASS = "3015"  # Hardcoded password
